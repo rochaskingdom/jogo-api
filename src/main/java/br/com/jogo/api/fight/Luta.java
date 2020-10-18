ccb32447de863067fd97e9b5168c05ad2b2a52ba
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-10-18 12:44:01 -0300

    Criacao da entidade Luta e migration

---
+++ src/main/java/br/com/jogo/api/fight/Luta.java
@@ -0,0 +1,68 @@
+package br.com.jogo.api.fight;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "LUTA")
+public class Luta {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CODIGO")
+    private Long codigo;
+
+    @NotNull
+    @CreationTimestamp
+    @Column(name = "LUTA_DATA")
+    public LocalDateTime lutaData;
+
+    @NotNull
+    @Column(name = "NOME_VENCEDOR")
+    public String nomeVencedor;
+
+    @NotNull
+    @Column(name = "LEVEL_VENCEDOR")
+    public int levelVencedor;
+
+    @NotNull
+    @Column(name = "IMAGEM_VENCEDOR")
+    public String imagemVencedor;
+
+    @NotNull
+    @Column(name = "NOME_PERDEDOR")
+    public String nomePerpededor;
+
+    @NotNull
+    @Column(name = "LEVEL_PERDEDOR")
+    public int levelPerpededor;
+
+    @NotNull
+    @Column(name = "IMAGEM_PERDEDOR")
+    public String imagemPerpededor;
+
+    @NotNull
+    @Column(name = "TIME_VENCEDOR")
+    public String timeVencedor;
+
+    @NotNull
+    @Column(name = "TIME_PERDEDOR")
+    public String timePerdedor;
+}
