f9e428df21888383da99e9efd1d84c3ae367dc79
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-10-18 12:45:02 -0300

    Criacao da controller Luta

---
+++ src/main/java/br/com/jogo/api/fight/LutaController.java
@@ -0,0 +1,45 @@
+package br.com.jogo.api.fight;
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/lutas")
+@Api(tags = "API - Lutas")
+public class LutaController {
+
+    @Autowired
+    private LutaService lutaService;
+
+    @GetMapping
+    public List<Luta> buscaTodasLutas() {
+        return lutaService.buscaTodasLutas();
+    }
+
+    @GetMapping("/aleatorio")
+    public ResponseEntity<Lutas> buscaLutaAleatorio() {
+        return lutaService.buscaLutaAleatoria();
+    }
+
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Luta> buscaLuta(@PathVariable Long codigo) {
+        return lutaService.buscaLuta(codigo);
+    }
+
+    @PostMapping
+    public Luta resultadoLuta(@RequestBody @Valid Lutas lutas) {
+        return lutaService.resultadoLuta(lutas);
+    }
+}
