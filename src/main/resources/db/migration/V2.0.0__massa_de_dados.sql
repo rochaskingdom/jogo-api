6d79f94962fda107b57a73a46ed09a7b18886f37
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-10-18 12:46:23 -0300

    Criacao de migration de massa de dados para testes

---
+++ src/main/resources/db/migration/V2.0.0__massa_de_dados.sql
@@ -0,0 +1,120 @@
+-- HEROIS
+INSERT INTO HEROI(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (1, 'Chewbacca', '',
+        'https://www.superherodb.com/pictures2/portraits/10/050/10466.jpg',
+        'Agility, Longevity, Marksmanship, Natural Weapons, Stealth, Super Strength, Weapons Master', 5);
+
+INSERT INTO HEROI(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (2, 'Angel Salvadore', 'Angel Salvadore Bohusk',
+        'https://www.superherodb.com/pictures2/portraits/10/050/1406.jpg',
+        'Animal Attributes, Animal Oriented Powers, Flight, Regeneration, Toxin and Disease Control', 4);
+
+INSERT INTO HEROI(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (3, 'Bill Harken', '', 'https://www.superherodb.com/pictures2/portraits/10/050/1527.jpg',
+        'Super Speed, Super Strength, Toxin and Disease Resistance', 6);
+
+INSERT INTO HEROI(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (4, 'Warlock (Infinity Gauntlet)', 'Adam Warlock',
+        'https://www.superherodb.com/pictures2/portraits/11/050/13426.jpg',
+        'Astral Projection, Biokinesis, Duplication, Element Control, Energy Beams, Energy Manipulation, Immortality, Matter Manipulation, Mind Control, Molecular Manipulation, Nigh-Omnipresent, Portal Creation, Reality Warping, Regeneration, Size Changing, Spatial Awareness, Super Speed, Super Strength, Telekinesis, Telepathy, Teleportation, Time Manipulation, Time Travel, Weather Control',
+        74);
+
+INSERT INTO HEROI(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (5, 'Shazam (New 52)', 'Billy Batson',
+        'https://www.superherodb.com/pictures2/portraits/10/050/14714.jpg',
+        'Accelerated Healing, Durability, Element Control, Energy Absorption, Energy Blasts, Fire Resistance, Flight, Illumination, Invulnerability, Levitation, Magic, Marksmanship, Super Speed, Super Strength',
+        14);
+
+INSERT INTO HEROI(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (6, 'Galadriel', '',
+        'https://www.superherodb.com/pictures2/portraits/11/050/11796.jpg',
+        'Danger Sense, Immortality, Intelligence, Invisibility, Magic, Precognition, Telekinesis, Telepathy', 17);
+
+INSERT INTO HEROI(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (7, 'Wonder Woman (DCEU)', 'Diana Prince',
+        'https://www.superherodb.com/pictures2/portraits/11/050/12529.jpg',
+        'Energy Absorption, Energy Manipulation, Enhanced Hearing, Enhanced Memory, Enhanced Senses, Enhanced Sight, Enhanced Smell, Enhanced Touch, Flight, Immortality, Regeneration, Super Speed, Super Strength, Telekinesis',
+        14);
+
+INSERT INTO HEROI(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (8, 'Wong (MCU)', 'Wong',
+        'https://www.superherodb.com/pictures2/portraits/11/050/13900.jpg',
+        'Astral Projection, Astral Travel, Dimensional Travel, Force Fields, Illusions, Magic, Teleportation', 7);
+
+INSERT INTO HEROI(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (9, 'Shikamaru', 'Shikamaru Nara',
+        'https://www.superherodb.com/pictures2/portraits/10/050/11742.jpg',
+        'Adaptation, Agility, Element Control, Fire Control, Intelligence, Jump, Marksmanship, Possession, Reflexes, Shapeshifting, Stamina, Stealth, Telekinesis, Wallcrawling, Weapon-based Powers, Weapons Master',
+        11);
+
+INSERT INTO HEROI(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (10, 'John Hartigan', 'John Hartigan',
+        'https://www.superherodb.com/pictures2/portraits/11/050/11229.jpg',
+        'Dexterity, Intelligence, Marksmanship, Reflexes, Stamina, Stealth, Weapons Master', 3);
+
+-- VILOES
+INSERT INTO VILAO(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (1, 'Buuccolo', 'Majin Buu', 'https://www.superherodb.com/pictures2/portraits/11/050/15355.jpg',
+        'Accelerated Healing, Adaptation, Agility, Flight, Immortality, Intelligence, Invulnerability, Reflexes, Self-Sustenance, Size Changing, Spatial Awareness, Stamina, Stealth, Super Breath, Super Speed, Super Strength, Teleportation',
+        22);
+
+INSERT INTO VILAO(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (2, 'Darth Vader', 'Anakin Skywalker', 'https://www.superherodb.com/pictures2/portraits/10/050/10444.jpg',
+        'Accelerated Healing, Agility, Astral Projection, Cloaking, Danger Sense, Durability, Electrokinesis, Energy Blasts, Enhanced Hearing, Enhanced Senses, Force Fields, Hypnokinesis, Illusions, Intelligence, Jump, Light Control, Marksmanship, Precognition, Psionic Powers, Reflexes, Stealth, Super Speed, Telekinesis, Telepathy, The Force, Weapons Master',
+        13);
+
+INSERT INTO VILAO(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (3, 'The Rival (CW)', 'Edward Clariss', 'https://www.superherodb.com/pictures2/portraits/11/050/13846.jpg',
+        'Accelerated Healing, Agility, Bullet Time, Durability, Electrokinesis, Endurance, Enhanced Senses, Intangibility, Marksmanship, Phasing, Reflexes, Speed Force, Stamina, Super Speed, Super Strength',
+        10);
+
+INSERT INTO VILAO(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (4, 'William Stryker (FOX)', 'William Stryker, Jr.',
+        'https://www.superherodb.com/pictures2/portraits/10/050/13957.jpg',
+        'Intelligence, Intuitive aptitude, Marksmanship, Weapon-based Powers', 9);
+
+INSERT INTO VILAO(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (5, 'Annihilus', 'Annihilus', 'https://www.superherodb.com/pictures2/portraits/10/050/1307.jpg',
+        'Agility, Durability, Flight, Reflexes, Stamina, Super Speed, Super Strength', 8);
+
+INSERT INTO VILAO(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (6, 'Angel Salvadore (FOX)', 'Angel Salvadore',
+        'https://www.superherodb.com/pictures2/portraits/11/050/14367.jpg',
+        'Animal Attributes, Animal Oriented Powers, Energy Blasts, Flight, Regeneration, Toxin and Disease Control', 3);
+
+INSERT INTO VILAO(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (7, 'Laufey (MCU)', 'Laufey', 'https://www.superherodb.com/pictures2/portraits/11/050/13974.jpg',
+        'Cold Resistance, Cryokinesis, Durability, Invulnerability, Longevity, Super Strength', 7);
+
+INSERT INTO VILAO(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (8, 'Tobias Whale (CW)', 'Tobias Whale', 'https://www.superherodb.com/pictures2/portraits/11/050/15894.jpg',
+        'Durability, Endurance, Longevity, Marksmanship, Stamina, Super Strength', 5);
+
+INSERT INTO VILAO(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (9, 'Towa', 'Towa', 'https://www.superherodb.com/pictures2/portraits/10/050/14841.jpg',
+        'Agility, Biokinesis, Durability, Energy Armor, Energy Beams, Energy Blasts, Energy Manipulation, Enhanced Senses, Flight, Force Fields, Intelligence, Magic, Mind Control, Portal Creation, Possession, Power Augmentation, Power Nullifier, Power Sense, Reflexes, Resurrection, Self-Sustenance, Size Changing, Stamina, Summoning, Super Speed, Super Strength, Teleportation, Time Travel, Umbrakinesis',
+        35);
+
+INSERT INTO VILAO(CODIGO, NOME, OUTRO_NOME, IMAGEM, PODERES, LEVEL)
+VALUES (10, 'Hela (MCU)', 'Hela Odinsdottir', 'https://www.superherodb.com/pictures2/portraits/11/050/12595.jpg',
+        'Accelerated Healing, Darkforce Manipulation, Durability, Elasticity, Longevity, Matter Manipulation, Portal Creation, Reflexes, Regeneration, Super Speed, Super Strength, Telekinesis',
+        17);
+
+-- LUTA
+INSERT INTO LUTA(CODIGO, LUTA_DATA, NOME_VENCEDOR, LEVEL_VENCEDOR, IMAGEM_VENCEDOR, NOME_PERDEDOR, LEVEL_PERDEDOR,
+                 IMAGEM_PERDEDOR, TIME_VENCEDOR, TIME_PERDEDOR)
+VALUES (1, current_timestamp, 'Chewbacca', 5,
+        'https://www.superherodb.com/pictures2/portraits/10/050/10466.jpg', 'Buuccolo', 3,
+        'https://www.superherodb.com/pictures2/portraits/11/050/15355.jpg', 'heroes', 'villains');
+
+INSERT INTO LUTA(CODIGO, LUTA_DATA, NOME_VENCEDOR, LEVEL_VENCEDOR, IMAGEM_VENCEDOR, NOME_PERDEDOR, LEVEL_PERDEDOR,
+                 IMAGEM_PERDEDOR, TIME_VENCEDOR, TIME_PERDEDOR)
+VALUES (2, current_timestamp, 'Galadriel', 10,
+        'https://www.superherodb.com/pictures2/portraits/11/050/11796.jpg', 'Darth Vader', 8,
+        'https://www.superherodb.com/pictures2/portraits/10/050/10444.jpg', 'heroes', 'villains');
+
+INSERT INTO LUTA(CODIGO, LUTA_DATA, NOME_VENCEDOR, LEVEL_VENCEDOR, IMAGEM_VENCEDOR, NOME_PERDEDOR, LEVEL_PERDEDOR,
+                 IMAGEM_PERDEDOR, TIME_VENCEDOR, TIME_PERDEDOR)
+VALUES (3, current_timestamp, 'Annihilus', 23,
+        'https://www.superherodb.com/pictures2/portraits/10/050/1307.jpg', 'Shikamaru', 1,
+        'https://www.superherodb.com/pictures2/portraits/10/050/11742.jpg', 'villains', 'heroes');
